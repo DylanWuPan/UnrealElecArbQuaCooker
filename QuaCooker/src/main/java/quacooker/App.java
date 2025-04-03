18b9f2283695de64d7cf2eddd1579623f25924fa
Dylan Pan <dylanpan56@gmail.com>
2025-04-02 23:20:23 -0400

    mac setup

--- QuaCooker/src/main/java/quacooker/App.java
+++ QuaCooker/src/main/java/quacooker/App.java
@@ -1,16 +1,12 @@
 package quacooker;
 
-import quacooker.api.CoinbaseWebSocketClient;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import quacooker.api.DataFetcher;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("QUA COOKIN");
 
-        DataFetcher dataFetcher = new DataFetcher(new String[] {"BTC-USD", "ETH-USD"});
+        DataFetcher dataFetcher = new DataFetcher(new String[] { "BTC-USD", "ETH-USD" });
 
         dataFetcher.fetchLiveData();
     }
