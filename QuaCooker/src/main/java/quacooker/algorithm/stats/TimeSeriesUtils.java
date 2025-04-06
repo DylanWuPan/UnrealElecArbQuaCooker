660520b3eac1cbd3a6e6c032b1ea503d78630a65
Quinn <quinn.thomson@roxburylatin.org>
2025-04-06 15:19:03 -0400

    Started working on utility methods

--- QuaCooker/src/main/java/quacooker/algorithm/stats/TimeSeriesUtils.java
+++ QuaCooker/src/main/java/quacooker/algorithm/stats/TimeSeriesUtils.java
@@ -5,6 +5,50 @@ package quacooker.algorithm.stats;
  * rolling window calculations.
  */
 public class TimeSeriesUtils {
+
+    // --- Statistical Methods ---
+
+    public static double calculateMean(double[] data) {
+        double mean = 0;
+
+        return mean;
+    }
+
+    public static double calculateStandardDeviation(double[] data) {
+        double sd = 0;
+
+        return sd;
+    }
+
+    public static double calculateCorrelation(double[] series1, double[] series2) {
+        double correlation = 0;
+
+        return correlation;
+    }
+
+    public static double[] calculateZScores(double[] data) {
+        double[] ZScores = {0, 1};
+
+        return ZScores;
+    }
+    
+
+    
+    // --- Moving Averages ---
+
+    public static double[] calculateEMA(double[] data, int period) {
+        double[] EMA = {0, 1};
+
+
+        return EMA;
+    }
+
+    public static double[] calculateSMA(double[] data, int period) {
+        double[] SMA = {0, 1};
+
+
+        return SMA;
+    }
     
 
 }
