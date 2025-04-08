935bcbb4b3298e58b95c420a22e5089ce286ab82
Quinn <quinn.thomson@roxburylatin.org>
2025-04-08 10:09:23 -0400

    updates to time series utils

--- QuaCooker/src/main/java/quacooker/algorithm/stats/TimeSeriesUtils.java
+++ QuaCooker/src/main/java/quacooker/algorithm/stats/TimeSeriesUtils.java
@@ -9,17 +9,62 @@ import java.util.ArrayList;
  */
 public class TimeSeriesUtils {
     public static ArrayList<Double> pricesToReturns(ArrayList<Double> prices) {
-        ArrayList<Double> returns = new ArrayList<>();
+    ArrayList<Double> returns = new ArrayList<>();
 
-        for (int i = 1; i < prices.size(); i++) {
-            double previousPrice = prices.get(i - 1);
-            double currentPrice = prices.get(i);
+    for (int i = 1; i < prices.size(); i++) {
+      double previousPrice = prices.get(i - 1);
+      double currentPrice = prices.get(i);
 
-            double returnValue = (currentPrice - previousPrice) / previousPrice * 100;
+      // Calculate the percentage return
+      double returnValue = (currentPrice - previousPrice) / previousPrice * 100;
 
-            returns.add(returnValue);
-        }
-
-        return returns;
+      // Add the return to the list
+      returns.add(returnValue);
     }
+
+    return returns;
+  }
+
+  public static double calculateMean(double[] data) {
+    double mean = 0;
+
+    return mean;
+}
+
+public static double calculateStandardDeviation(double[] data) {
+    double sd = 0;
+
+    return sd;
+}
+
+public static double calculateCorrelation(double[] series1, double[] series2) {
+    double correlation = 0;
+
+    return correlation;
+}
+
+public static double[] calculateZScores(double[] data) {
+    double[] ZScores = {0, 1};
+
+    return ZScores;
+}
+
+
+
+// --- Moving Averages ---
+
+public static double[] calculateEMA(double[] data, int period) {
+    double[] EMA = {0, 1};
+
+
+    return EMA;
+}
+
+public static double[] calculateSMA(double[] data, int period) {
+    double[] SMA = {0, 1};
+
+
+    return SMA;
+}
+  
 }
