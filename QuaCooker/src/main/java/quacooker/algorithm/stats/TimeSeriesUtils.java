fc3cb8778757710a13471e112d7d15fa528ea864
Dylan Pan <dylanpan56@gmail.com>
2025-04-08 10:48:31 -0400

    remove .class files

--- QuaCooker/src/main/java/quacooker/algorithm/stats/TimeSeriesUtils.java
+++ QuaCooker/src/main/java/quacooker/algorithm/stats/TimeSeriesUtils.java
@@ -8,64 +8,60 @@ import java.util.ArrayList;
  * rolling window calculations.
  */
 public class TimeSeriesUtils {
-    
+
     public static ArrayList<Double> pricesToReturns(ArrayList<Double> prices) {
-    ArrayList<Double> multiplicativeReturns = new ArrayList<>();
+        ArrayList<Double> multiplicativeReturns = new ArrayList<>();
 
-    for (int i = 1; i < prices.size(); i++) {
-      double previousPrice = prices.get(i - 1);
-      double currentPrice = prices.get(i);
+        for (int i = 1; i < prices.size(); i++) {
+            double previousPrice = prices.get(i - 1);
+            double currentPrice = prices.get(i);
 
-      // Calculate the percentage return (multiplicative)
-      double returnValue = (currentPrice - previousPrice) / previousPrice * 100;
+            // Calculate the percentage return (multiplicative)
+            double returnValue = (currentPrice - previousPrice) / previousPrice * 100;
 
-      // Add the return to the list
-      multiplicativeReturns.add(returnValue);
+            // Add the return to the list
+            multiplicativeReturns.add(returnValue);
+        }
+
+        return multiplicativeReturns;
     }
 
-    return multiplicativeReturns;
-  }
+    public static double calculateMean(double[] data) {
+        double mean = 0;
 
-  public static double calculateMean(double[] data) {
-    double mean = 0;
+        return mean;
+    }
 
-    return mean;
-}
+    public static double calculateStandardDeviation(double[] data) {
+        double sd = 0;
 
-public static double calculateStandardDeviation(double[] data) {
-    double sd = 0;
+        return sd;
+    }
 
-    return sd;
-}
+    public static double calculateCorrelation(double[] series1, double[] series2) {
+        double correlation = 0;
 
-public static double calculateCorrelation(double[] series1, double[] series2) {
-    double correlation = 0;
+        return correlation;
+    }
 
-    return correlation;
-}
+    public static double[] calculateZScores(double[] data) {
+        double[] ZScores = { 0, 1 };
 
-public static double[] calculateZScores(double[] data) {
-    double[] ZScores = {0, 1};
+        return ZScores;
+    }
 
-    return ZScores;
-}
+    // --- Moving Averages ---
 
+    public static double[] calculateEMA(double[] data, int period) {
+        double[] EMA = { 0, 1 };
 
+        return EMA;
+    }
 
-// --- Moving Averages ---
+    public static double[] calculateSMA(double[] data, int period) {
+        double[] SMA = { 0, 1 };
 
-public static double[] calculateEMA(double[] data, int period) {
-    double[] EMA = {0, 1};
+        return SMA;
+    }
 
-
-    return EMA;
-}
-
-public static double[] calculateSMA(double[] data, int period) {
-    double[] SMA = {0, 1};
-
-
-    return SMA;
-}
-  
 }
